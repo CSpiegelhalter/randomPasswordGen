fd0db3d5d0bf522edb85cc8d8fbe41d7cd9bf28a
Curt Spiegelhalter <i16dcurt@gmail.com>
2020-12-17 00:13:08 -0600

    Fixed function recursion

--- script.js
+++ script.js
@@ -12,18 +12,12 @@ function writePassword() {
 
 function generatePassword() {
 
-  // Ask user password length
-  var length = parseInt(prompt("How long would you like your password (8-128)"));
-  
-  // Check to see if password length and input type is valid
-  if ((length < 8) || (length > 128) || (Number.isInteger(length) == false)) {
-    alert("Your password needs to be 8-128 characters long");
-    generatePassword();
-  }
-  else {
-    // Call function to get password contents
-    var options = passwordOptions()
-  }
+  // Call function to get password contents
+  var options = passwordOptions();
+
+  // Separates the returned array
+  var length = options[0]
+  var parameters = options[1]
 
   // Create empty string to add password characters to
   var myPassword = "";
@@ -32,7 +26,7 @@ function generatePassword() {
   for (var i = 0; i < length; i++) {
 
     // Randomly select a character in the parameters string with the 'charAt" method then add it to myPassword string
-    myPassword += options.charAt(Math.floor(Math.random() * options.length));
+    myPassword += parameters.charAt(Math.floor(Math.random() * parameters.length));
   }
   
   // Return password value
@@ -43,37 +37,59 @@ function generatePassword() {
 function passwordOptions() {
 
   // Create empty string to add password parameters to
-  var parameters = "";
+  let parameters = "";
 
-  // Decide whether or not to add character types
-  var lower = confirm("Would you like lower case characters in it?");
-  
-  if(lower) {
-    parameters += "abcdefghijklmnopqrstuvwxyz";
+  // Ask user password length
+  let length = parseInt(prompt("How long would you like your password (8-128)"));
+
+  // Check to see if password length and input type is valid
+  if ((length < 8) || (length > 128) || (Number.isInteger(length) == false)) {
+    alert("Your password needs to be 8-128 characters long");
+    return passwordOptions();
   }
+  else {
 
-  var upper = confirm("Would you like upper case characters in it?");
+    // Decide whether or not to add character types
+    let lower = confirm("Would you like lower case characters in it?");
+    
+    // All lower case characters
+    if(lower) {
+      parameters += "abcdefghijklmnopqrstuvwxyz";
+    }
 
-  if(upper) {
-    parameters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let upper = confirm("Would you like upper case characters in it?");
+
+    // All upper case characters
+    if(upper) {
+      parameters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    let numeric = confirm("Would you like numeric characters in it?");
+
+    // All numbers
+    if(numeric) {
+      parameters += "0123456789";
+    }
+
+    let special = confirm("Would you like special characters in it?");
+
+    // All special characters
+    // Must add '\' escape character to two characters that would break it otherwise
+    if(special) {
+      parameters += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    }
+
+    // Ensures at least one parameter was selected
+    if (parameters == "") {
+      alert("You must select at least one password parameter. Try again.");
+      return passwordOptions();
+    }
+
+    // Returns the user input
+    else{
+      return [length, parameters];
+    }
   }
-
-  var numeric = confirm("Would you like numeric characters in it?");
-
-  if(numeric) {
-    parameters += "0123456789";
-  }
-
-  var special = confirm("Would you like special characters in it?");
-
-  // Must add '\' escape character to two characters that would break it otherwise
-  if(special) {
-    parameters += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-  }
-
-  return parameters
-
-
 }
 
 // Add event listener to generate button
